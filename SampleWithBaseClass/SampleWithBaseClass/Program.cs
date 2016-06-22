b09c905849c7d47ff28fc638f940c44556074cb6
goi <goiwebdev@gmail.com>
2016-06-22 20:54:01 +0800

    added

---
+++ SampleWithBaseClass/SampleWithBaseClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWithBaseClass
+{
+    class Net
+    {
+        public int _value = 6;
+    }
+
+    class Perl : Net
+    {
+        public new int _value = 7;
+        public void write()
+        {
+            Console.WriteLine(base._value);
+            Console.WriteLine(this._value);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Perl perl = new Perl();
+            perl.write();
+            Console.ReadLine();
+
+        }
+
+    }
+}
